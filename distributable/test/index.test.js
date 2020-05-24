eb33ab190f7d279d21111e2b943811ad0180163e
virtualpatterns <code.virtualpatterns@gmail.com>
2020-05-23 22:04:04 -0400

    post-test

--- distributable/test/index.test.js
+++ distributable/test/index.test.js
@@ -1,16 +1,6 @@
 import Test from 'ava';
 
-Test.before(async test => {
-  test.context.index = await import('../index.js');
-});
-
-[
-'Bundle'].
-forEach(name => {
-
-  Test(name, async test => {
-    test.truthy(test.context.index[name]);
-  });
-
+Test('Bundle', async test => {
+  test.truthy((await import('../index.js')).Bundle);
 });
 //# sourceMappingURL=index.test.js.map
