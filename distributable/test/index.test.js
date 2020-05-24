75dcd049f2bd21d63f6def5c24fb2e3ed87c3c8a
virtualpatterns <code.virtualpatterns@gmail.com>
2020-05-23 22:51:34 -0400

    post-test

--- distributable/test/index.test.js
+++ distributable/test/index.test.js
@@ -1,6 +1,21 @@
+import Is from '@pwn/is';
 import Test from 'ava';
 
 Test('Bundle', async test => {
   test.truthy((await import('../index.js')).Bundle);
+});
+
+Test('Is.functionOrAsyncFunction(value)', async test => {
+
+  let { Bundle } = await import('../index.js');
+
+  Is.use(Bundle);
+
+  test.true(Is.functionOrAsyncFunction(() => {}));
+  test.true(Is.functionOrAsyncFunction(async () => {}));
+
+  test.false(Is.functionOrAsyncFunction('() => {}'));
+  test.false(Is.functionOrAsyncFunction('async () => {}'));
+
 });
 //# sourceMappingURL=index.test.js.map
