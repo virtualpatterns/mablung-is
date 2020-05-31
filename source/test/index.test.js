321f31cbaa1495169ec3306471b306ba05d2261f
virtualpatterns <code.virtualpatterns@gmail.com>
2020-05-30 23:03:58 -0400

    upgraded, added windows predicate

--- source/test/index.test.js
+++ source/test/index.test.js
@@ -1,13 +1,23 @@
+import IsWindows from 'is-windows'
 import Test from 'ava'
 
 import { Is } from '../index.js'
 
-Test('Is.functionOrAsyncFunction(value)', async (test) => {
+Test('Is.functionOrAsyncFunction(value)', (test) => {
 
   test.true(Is.functionOrAsyncFunction(() => {}))
   test.true(Is.functionOrAsyncFunction(async () => {}))
 
   test.false(Is.functionOrAsyncFunction('() => {}'))
   test.false(Is.functionOrAsyncFunction('async () => {}'))
+
+})
+
+Test('Is.windows()', (test) => {
+
+  let isWindows = Is.windows()
+
+  test.log(`Is.windows() = ${Is.windows()}`)
+  test.is(isWindows, IsWindows())
 
 })
