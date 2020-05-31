6e40235f083a0a8928fd362f36d0fb6c7f9a9f8b
virtualpatterns <code.virtualpatterns@gmail.com>
2020-05-30 23:05:15 -0400

    post-test

--- distributable/library/bundle.js
+++ distributable/library/bundle.js
@@ -1,7 +1,13 @@
+import IsWindows from 'is-windows';
+
 function Bundle(utility) {
 
   utility.addPredicate('functionOrAsyncFunction', value => {
     return typeof value === 'function';
+  });
+
+  utility.addPredicate('windows', () => {
+    return IsWindows();
   });
 
 }
